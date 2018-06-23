f6cad28402c2e6602671d7ddf55e35e9d6b7c2b4
Thiago Morgado <Thiago-morgado@outlook.com.br>
2018-06-22 23:05:57 -0300

    Add project files.

---
+++ PlayGames/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlayGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(1088, 584);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PlayGames";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(1141, 623);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "V.1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PlayGames.Properties.Resources.super_mario_world_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1386, 740);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PlayGames/Form1.cs
@@ -0,0 +1,42 @@
+﻿using PlayGames.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayGames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.BackgroundImage = Properties.Resources.super_mario_world_2;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer tm = new Timer();
+            tm.Interval = 3000;
+            tm.Tick += new EventHandler(changeImage);
+            tm.Start();
+        }
+
+        private void changeImage(object sender, EventArgs e)
+        {
+            List<Bitmap> b1 = new List<Bitmap>();
+            b1.Add(Resources.super_mario_world_2);
+            b1.Add(Resources.maxresdefault_9);
+            b1.Add(Resources.super_mario_all_stars___super_mario_world_hd_wallpapers_33717_794428);
+            b1.Add(Resources._1407976_mario_kart_8);
+
+            int index = DateTime.Now.Second % 4;
+            this.BackgroundImage = b1[index];
+        }
+    }
+}
